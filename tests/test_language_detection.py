902f6ac69c8b59c291c5c46f2c24cc5fff3c99a1
MouslyDiaw <mouslydiaw@gmail.com>
2020-05-14 17:14:08 +0200

    Add test case for output

--- tests/test_language_detection.py
+++ tests/test_language_detection.py
@@ -11,7 +11,7 @@ def text_example():
     return text
 
 
-def test_language_detector_fasttext_format(text_example):
+def test_language_detector_format(text_example):
     """
     Test output format.
     """
@@ -20,3 +20,15 @@ def test_language_detector_fasttext_format(text_example):
     detected_lang_batch = langdetect([text_example])
     assert isinstance(detected_lang_single, str)
     assert isinstance(detected_lang_batch, list)
+
+
+def test_language_detector_langdetect_output(text_example):
+    """
+    Test output result.
+    """
+
+    detected_lang_single = langdetect(text_example)
+    detected_lang_batch = langdetect([text_example])
+    assert detected_lang_single == "fr"
+    assert detected_lang_batch == ["fr"]
+    assert len(detected_lang_batch) == 1
