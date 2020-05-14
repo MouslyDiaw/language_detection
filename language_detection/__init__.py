ca20a283305d2ff669904ac7a701e63279f6790b
MouslyDiaw <mouslydiaw@gmail.com>
2020-05-14 15:56:45 +0200

    Fix path director fro loading model

--- language_detection/__init__.py
+++ language_detection/__init__.py
@@ -8,7 +8,7 @@ __email__ = 'mouslydiaw@gmail.com'
 __version__ = '0.1.0'
 
 
-ROOT_DIR = os.path.abspath(__file__)
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_PATH = os.path.join(ROOT_DIR, "models", "lid.176.ftz")
 FT_MODEL_LANGUAGE = fasttext.load_model(MODEL_PATH)
