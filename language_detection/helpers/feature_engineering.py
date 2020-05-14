6de55e9b7b0945db05bf16f0dd5f0aa1e64c17a9
MouslyDiaw <mouslydiaw@gmail.com>
2020-05-14 15:37:26 +0200

    Add scripts

---
+++ language_detection/helpers/feature_engineering.py
@@ -0,0 +1,16 @@
+""" Pre-processing text """
+
+import re
+
+
+def remove_newline(text: str) -> str:
+    """ Delete newlines
+
+    Args:
+        text (str): text to sanitize
+
+    Returns:
+        str: text without non-alphanumeric character, html tags, email, etc.
+
+    """
+    return re.sub(r"\n+", "", text)
--- language_detection/language_detection.py
+++ language_detection/language_detection.py
@@ -1,1 +1,31 @@
-"""Main module."""
+#!/usr/bin/env python3
+""" Module for Language detection """
+import logging
+
+
+from language_detection import FT_MODEL_LANGUAGE
+from language_detection.helpers.feature_engineering import remove_newline
+
+
+def langdetect(text: str) -> str:
+    """ language detection with fasttext (pre-trained model)
+
+    Args:
+        text (str): sentence to detect language
+
+    Returns:
+        str: code language ex: 'fr', 'en', 'de', ...
+
+    Notes:
+        - Fasttext language identification is sensitive to punctuation.
+        - Input doesn't contains any punctuation (aka \\n)
+        - ValueError: we raise an error if it occurs any \n in text
+    """
+    sanitized_text = remove_newline(text)
+    try:
+        ft_pred = FT_MODEL_LANGUAGE.predict(sanitized_text, k=1, threshold=0.0)
+        return ft_pred[0][0].split("__label__")[-1]
+    except ValueError:
+        logging.exception("Input doesn't contain `\\n`")
+    except AttributeError:
+        logging.exception(f"Input must be a str but you give {text}")
