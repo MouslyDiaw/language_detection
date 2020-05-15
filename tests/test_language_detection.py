d47df388178d2e6acdcd2b7ed957e8a005185c2b
MouslyDiaw <mouslydiaw@gmail.com>
2020-05-15 17:55:36 +0200

    Add scope for fixture

--- tests/test_language_detection.py
+++ tests/test_language_detection.py
@@ -3,7 +3,7 @@ import pytest
 from language_detection.langdetector import langdetect
 
 
-@pytest.fixture
+@pytest.fixture(scopre="module")
 def text_example():
     text = "JobTeaser est une entreprise française qui fournit des solutions de recrutement et marque employeur aux \
     entreprises pour embaucher des jeunes talents et une plateforme carrières gratuite pour les services carrières \
